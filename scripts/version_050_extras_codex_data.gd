b2e8e4c7fba2f86ec1b9b4450503631bbef68eb8
javidei <javyspot@gmail.com>
2026-08-11 11:18:58 +0200

    fix: desacoplar Extras del identificador global Autoload

--- scripts/version_050_extras_codex_data.gd
+++ scripts/version_050_extras_codex_data.gd
@@ -1,8 +1,11 @@
 extends "res://scripts/version_050_extras_codex.gd"
 
+const DataAccess = preload("res://scripts/data_access.gd")
+
 
 func _load_data() -> void:
-	data = DataManager.get_codex_data()
+	var dm: Variant = DataAccess.dm()
+	data = dm.call("get_codex_data") if dm != null else {}
 	characters = []
 	var people: Variant = data.get("personajes", [])
 	if typeof(people) == TYPE_ARRAY:
