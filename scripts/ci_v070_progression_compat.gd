ab9efece68b3e679eaf0e052868b8f7da28cbdd6
javidei <javyspot@gmail.com>
2026-08-14 17:05:48 +0200

    test: retirar wrapper de compatibilidad redundante

--- scripts/ci_v070_progression_compat.gd
+++
@@ -1,9 +0,0 @@
-extends "res://scripts/ci_v069_progression_smoke_test.gd"
-
-# El smoke heredado 0.6 valida mecánicas que siguen vigentes en 0.7, pero su
-# guard clause exige literalmente una versión 0.6.x. Solo dentro de este proceso
-# headless presentamos 0.6.9 al test heredado; el proyecto/export real conserva
-# siempre su versión 0.7.x.
-func _initialize() -> void:
-	ProjectSettings.set_setting("application/config/version", "0.6.9")
-	super()
